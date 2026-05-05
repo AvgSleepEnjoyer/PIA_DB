4b835b52644dbffec00674ee3ef25f5cb4f2f3b3
Rebeca <rebecasanchez2513@gmail.com>
2026-05-05 17:40:22 -0600

    first comit -r

---
+++ conector.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",          # change if needed
+        password="12345",
+        database="appjuegos"
+    )
+
+def menu():
+    print("""
+        \t\tBASE DE DATOS: STEAM-SEQUEL\n
+        \t1. Mostrar catalogo\n
+        \t2. Registrar nuevo juego\n
+        \t3. Mostrar usuarios\n
+    """)
+
+def mostrarCatalogo():
+    print("""
+        \t\tMostrar por:
+        \t1.Clasificacion\n
+        \t2.Titulo\n
+        \t3.Precio\n
+        \t4.Fecha de salida\n
+
+    """)
+
+def get_games_by_year(year):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = "SELECT titulo, precio, fechaSalida FROM juegos WHERE fechaSalida = %s"
+    cursor.execute(query, (year,))  # safe parameter
+
+    results = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return results
